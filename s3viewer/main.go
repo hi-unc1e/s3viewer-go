d4ec03ec0e5eae5d5598132b4164b8a25fda20af
李含伟 <lihanwei@zhiqiansec.com>
2024-06-19 23:33:30 +0800

    go project just made me mad

---
+++ s3viewer/main.go
@@ -0,0 +1,35 @@
+package s3viewer
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	// 读取 XML 文件内容
+	fileText, err := ioutil.ReadFile("/Users/dpdu/Desktop/opt/s3view_dev/s3viewer-go/test/h2-html.xml")
+	if err != nil {
+		log.Fatalf("Failed to read XML file: %v", err)
+	}
+
+	// 提取 <ListBucketResult> 标签及其内容
+	xmlContent, err := findS3XMLString(string(fileText))
+	if err != nil {
+		log.Fatalf("Failed to find S3 XML string: %v", err)
+	}
+
+	// 预处理 XML 内容
+	xmlContent = sanitizeXMLContent(xmlContent)
+
+	// 解析 XML 内容为对象
+	result, err := parseXMLToListBucketResult(xmlContent)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal XML: %v", err)
+	}
+
+	// 打印解析后的对象
+	for _, file := range result.Files {
+		fmt.Printf("%v\n", file)
+	}
+}
---
+++ s3viewer/s3viewer_test.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/xml"
+	"testing"
+)
+
+const sampleXML = `
+<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+    <Name>example-bucket</Name>
+    <Prefix/>
+    <Marker/>
+    <MaxKeys>1000</MaxKeys>
+    <IsTruncated>false</IsTruncated>
+    <Contents>
+        <Key>test-file1.txt</Key>
+        <LastModified>2023-05-22T08:49:07.000Z</LastModified>
+        <ETag>"f19cd76cd7fac68d15f0c40a063519c9"</ETag>
+        <Size>1234</Size>
+        <Owner>
+            <DisplayName>owner</DisplayName>
+            <ID>owner-id</ID>
+        </Owner>
+        <StorageClass>STANDARD</StorageClass>
+    </Contents>
+</ListBucketResult>`
+
+const invalidXML = `
+<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+    <Name>example-bucket</Name>
+    <Prefix/>
+    <Marker/>
+    <MaxKeys>1000</MaxKeys>
+    <IsTruncated>false</IsTruncated>
+    <Contents>
+        <Key>test-file1.txt</Key>
+        <LastModified>2023-05-22T08:49:07.000Z</LastModified>
+        <ETag>"f19cd76cd7fac68d15f0c40a063519c9"</ETag>
+        <Size>1234</Size>
+        <Owner>
+            <DisplayName>owner</DisplayName>
+            <ID>owner-id</ID>
+        </Owner>
+        <StorageClass>STANDARD</StorageClass>
+    </Contents>
+    <Contents>
+        <Key>004-快速部署指南&安装手册/网康_日志中心 R4.4_x64_安装手册.docx</Key>
+        <LastModified>2023-05-22T08:49:07.000Z</LastModified>
+        <ETag>"f19cd76cd7fac68d15f0c40a063519c9"</ETag>
+        <Size>1234</Size>
+        <Owner>
+            <DisplayName>owner</DisplayName>
+            <ID>owner-id</ID>
+        </Owner>
+        <StorageClass>STANDARD</StorageClass>
+    </Contents>
+</ListBucketResult>`
+
+func TestFindS3XMLString(t *testing.T) {
+	xmlContent, err := findS3XMLString(sampleXML)
+	if err != nil {
+		t.Fatalf("Failed to find S3 XML string: %v", err)
+	}
+
+	var result ListBucketResult
+	err = xml.Unmarshal(xmlContent, &result)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal XML: %v", err)
+	}
+
+	if result.Name != "example-bucket" {
+		t.Errorf("Expected bucket name 'example-bucket', got '%s'", result.Name)
+	}
+}
+
+func TestSanitizeXMLContent(t *testing.T) {
+	xmlContent := sanitizeXMLContent([]byte(invalidXML))
+
+	var result ListBucketResult
+	err := xml.Unmarshal(xmlContent, &result)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal sanitized XML: %v", err)
+	}
+
+	if result.Name != "example-bucket" {
+		t.Errorf("Expected bucket name 'example-bucket', got '%s'", result.Name)
+	}
+}
+
+func TestParseXMLToListBucketResult(t *testing.T) {
+	xmlContent := sanitizeXMLContent([]byte(sampleXML))
+
+	result, err := parseXMLToListBucketResult(xmlContent)
+	if err != nil {
+		t.Fatalf("Failed to parse XML to ListBucketResult: %v", err)
+	}
+
+	if result.Name != "example-bucket" {
+		t.Errorf("Expected bucket name 'example-bucket', got '%s'", result.Name)
+	}
+
+	if len(result.Files) != 1 {
+		t.Fatalf("Expected 1 file, got %d", len(result.Files))
+	}
+
+	if result.Files[0].Key != "test-file1.txt" {
+		t.Errorf("Expected file key 'test-file1.txt', got '%s'", result.Files[0].Key)
+	}
+}
