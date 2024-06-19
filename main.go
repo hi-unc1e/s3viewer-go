be063ddb7a01a6efd919696e16b6a010e9d08f12
李含伟 <lihanwei@zhiqiansec.com>
2024-06-19 22:57:56 +0800

    xml -> obj

--- main.go
+++ main.go
@@ -3,57 +3,74 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"regexp"
 )
 
-// S3BucketListResponse 是 S3 XML 输出的结构映射
-type S3BucketListResponse struct {
-	XMLName  xml.Name `xml:"ListBucketResult"`
-	Name     string   `xml:"Name"`
-	Contents []struct {
-		Key          string `xml:"Key"`
-		LastModified string `xml:"LastModified"`
-		Size         int    `xml:"Size"`
-	} `xml:"Contents"`
+type ListBucketResult struct {
+	XMLName     xml.Name `xml:"ListBucketResult"`
+	Name        string   `xml:"Name"`
+	Prefix      string   `xml:"Prefix"`
+	Marker      string   `xml:"Marker"`
+	MaxKeys     int      `xml:"MaxKeys"`
+	IsTruncated bool     `xml:"IsTruncated"`
+	Files       []File   `xml:"Contents"`
+}
+
+type File struct {
+	Key          string `xml:"Key"`
+	LastModified string `xml:"LastModified"`
+	_            string `xml:"ETag"`
+	Size         int    `xml:"Size"`
+	_            Owner  `xml:"Owner"`
+	_            string `xml:"StorageClass"`
+}
+
+type Owner struct {
+	DisplayName string `xml:"DisplayName"`
+	ID          string `xml:"ID"`
+}
+
+func find_s3_xml_string(xml_content string) ([]byte, error) {
+	// 定义正则表达式来匹配 <ListBucketResult> 标签及其内容
+	re := regexp.MustCompile(`(?s)<ListBucketResult.*?</ListBucketResult>`)
+
+	// 查找匹配的内容
+	matches := re.FindAllString(xml_content, -1)
+
+	if len(matches) == 0 {
+		match := ""
+		return []byte(match), fmt.Errorf("No matches found")
+	} else {
+		fmt.Println(matches)
+		match := matches[0]
+		return []byte(match), nil
+	}
+
 }
 
 func main() {
-	// 假设 s3XML 是包含 XML 数据的字符串
-	s3XML := `...` // 这里应该是你的 XML 输出
-
-	// 解析 XML
-	var listResp S3BucketListResponse
-	err := xml.Unmarshal([]byte(s3XML), &listResp)
+	// 读取 XML 文件内容
+	file_text, err := ioutil.ReadFile("/Users/dpdu/Desktop/opt/s3view_dev/s3viewer-go/test/h2-html.xml")
 	if err != nil {
-		log.Fatalf("Error unmarshalling XML: %v", err)
+		log.Fatalf("Failed to read XML file: %v", err)
 	}
-
-	// 创建新的 Excel 文件
-	f := excelize.NewFile()
-
-	// 在 Excel 中创建一个新的工作表
-	sheet := "Sheet1"
-	f.SetSheetName("Sheet1", sheet)
-
-	// 设置表格的表头
-	headers := map[string]string{"A1": "文件名", "B1": "上次编辑日期", "C1": "大小"}
-	for col, val := range headers {
-		f.SetCellValue(sheet, col, val)
-	}
-
-	// 填充数据
-	for i, content := range listResp.Contents {
-		row := i + 2 // 从第二行开始填充数据，因为第一行是表头
-		f.SetCellValue(sheet, "A"+fmt.Sprint(row), content.Key)
-		f.SetCellValue(sheet, "B"+fmt.Sprint(row), content.LastModified)
-		f.SetCellValue(sheet, "C"+fmt.Sprint(row), content.Size)
-	}
-
-	// 保存表格
-	err = f.SaveAs("S3BucketList.xlsx")
+	xmlContent, err := find_s3_xml_string(string(file_text))
 	if err != nil {
-		log.Fatalf("无法保存文件, %v", err)
+		log.Fatalf("Failed to parse XML file: %v", err)
 	}
 
-	fmt.Println("Excel 表格已成功创建。")
+	// 解析 XML 内容为对象
+	var result ListBucketResult
+	err = xml.Unmarshal(xmlContent, &result)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal XML: %v", err)
+	}
+
+	// 打印解析后的对象
+	for _, file := range result.Files {
+		fmt.Printf("%v\n", file)
+	}
+
 }
