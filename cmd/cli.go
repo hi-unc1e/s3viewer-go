251c2c02a51fc4bf001ed6094765b9743ab8beb8
--replace-all <--replace-all>
2024-06-23 10:16:40 +0800

    move to cmd/

---
+++ cmd/cli.go
@@ -0,0 +1,9 @@
+package s3viewer
+
+import (
+	s3 "github.com/hi-unc1e/s3viewer-go/s3viewer"
+)
+
+func main() {
+	s3.LoadFile("/Users/dpdu/Desktop/opt/s3view_dev/s3viewer-go/test/h1.xml")
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "s3viewer-go/s3viewer"
-
-func main() {
-	s3viewer.LoadFile("/Users/dpdu/Desktop/opt/s3view_dev/s3viewer-go/test/h1.xml")
-}
